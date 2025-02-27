71632bf59a3436887c837f4d167cb81a34396902
tranmanhduy <n22dccn114@student.ptithcm.edu.vn>
2025-02-27 22:00:29 +0700

    update

---
+++ CNPM_TTCS_QLGX/src/main/java/com/nmcnpm_ttcs/cnpm_ttcs_qlgx/CNPM_TTCS_QLGX.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.nmcnpm_ttcs.cnpm_ttcs_qlgx;
+
+/**
+ *
+ * @author manhh
+ */
+public class CNPM_TTCS_QLGX {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
