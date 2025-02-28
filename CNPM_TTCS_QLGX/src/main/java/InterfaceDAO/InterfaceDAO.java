d00afc84a76404b934ac11dc60ec357e212067f7
vudkhoa <vudinhkhoaowl@gmail.com>
2025-02-28 13:52:26 +0700

    update Model Package

--- CNPM_TTCS_QLGX/src/main/java/InterfaceDAO/InterfaceDAO.java
+++ CNPM_TTCS_QLGX/src/main/java/InterfaceDAO/InterfaceDAO.java
@@ -1,13 +1,15 @@
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
  */
 package InterfaceDAO;
+
+import java.util.ArrayList;
 
 /**
  *
  * @author Admin
  */
-public class InterfaceDAO {
-    
+public interface InterfaceDAO<T> {
+    public ArrayList<T> getList();
 }
