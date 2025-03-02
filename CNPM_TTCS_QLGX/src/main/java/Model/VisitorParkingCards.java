5c40bbd4a8146e630a8862ae7655cdb80b8171a0
ligten000 <161305515+ligten000@users.noreply.github.com>
2025-03-02 22:36:12 +0700

    add DAO

--- CNPM_TTCS_QLGX/src/main/java/Model/VisitorParkingCards.java
+++ CNPM_TTCS_QLGX/src/main/java/Model/VisitorParkingCards.java
@@ -9,5 +9,31 @@ package Model;
  * @author HP
  */
 public class VisitorParkingCards {
+    int visitor_parking_card_id;
+    boolean is_active;
+
+    public VisitorParkingCards() {
+    }
+
+    public VisitorParkingCards(int visitor_parking_card_id, boolean is_active) {
+        this.visitor_parking_card_id = visitor_parking_card_id;
+        this.is_active = is_active;
+    }
+
+    public int getVisitor_parking_card_id() {
+        return visitor_parking_card_id;
+    }
+
+    public boolean isIs_active() {
+        return is_active;
+    }
+
+    public void setIs_active(boolean is_active) {
+        this.is_active = is_active;
+    }
+
+    public void setVisitor_parking_card_id(int visitor_parking_card_id) {
+        this.visitor_parking_card_id = visitor_parking_card_id;
+    }
     
 }
