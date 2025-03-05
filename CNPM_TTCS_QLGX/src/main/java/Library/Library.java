40090271b0e9628aa9abc8c63e4d40fab2e25f30
nennosleep <vanntrong7890@gmail.com>
2025-03-05 19:29:03 +0700

    Update Library.java

--- CNPM_TTCS_QLGX/src/main/java/Library/Library.java
+++ CNPM_TTCS_QLGX/src/main/java/Library/Library.java
@@ -5,6 +5,9 @@
 package Library;
 
 import java.text.NumberFormat;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeParseException;
 import java.util.Locale;
 
 /**
@@ -28,11 +31,32 @@ public class Library {
         return str.matches("\\d{12}");
     }
     
+    public static boolean isValidPhoneNumber(String phone_number) {
+        return phone_number != null && phone_number.matches("0\\d{9}");
+    }
+    
     // in dinh dang tien te
     public static String formatCurrency(int amount) {
         NumberFormat formatter = NumberFormat.getInstance(Locale.US); // Định dạng theo US (1,000,000)
         return formatter.format(amount);
     }
+    
+    // Kiểm tra ngày sinh có đúng định dạng và không lớn hơn ngày hiện tại
+    public static boolean isValidDateOfBirth(LocalDate dateOfBirth) {
+        return dateOfBirth != null && !dateOfBirth.isAfter(LocalDate.now());
+    }
+
+    // Chuyển đổi chuỗi "dd/MM/yyyy" thành LocalDate
+    public static LocalDate parseDateOfBirth(String dateStr) {
+        if (dateStr == null || dateStr.isEmpty()) return null;
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+        try {
+            return LocalDate.parse(dateStr, formatter);
+        } catch (DateTimeParseException e) {
+            return null;
+        }
+    }
+
     
     public static void main(String[] args) {
         
