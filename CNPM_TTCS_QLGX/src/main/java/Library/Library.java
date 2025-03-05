4d01be9ca854107b0d18a61a77f270ac9e57a22c
tranmanhduy <n22dccn114@student.ptithcm.edu.vn>
2025-03-05 18:41:02 +0700

    create some function

--- CNPM_TTCS_QLGX/src/main/java/Library/Library.java
+++ CNPM_TTCS_QLGX/src/main/java/Library/Library.java
@@ -4,10 +4,37 @@
  */
 package Library;
 
+import java.text.NumberFormat;
+import java.util.Locale;
+
 /**
  *
  * @author manhh
  */
 public class Library {
+    // Ten chi gom a - z, A - Z
+    public static boolean isValidString(String str) {
+        return str.matches("[a-zA-Z ]+");
+    }
     
+    // email validate
+    public static boolean isValidEmail(String email) {
+        String emailRegex = "^[a-zA-Z0-9]+@[a-zA-Z0-9.]+\\.[a-zA-Z]{2,}$";
+        return email.matches(emailRegex);
+    }
+    
+    // kiem tra cccd
+    public static boolean isValidSSN(String str) {
+        return str.matches("\\d{12}");
+    }
+    
+    // in dinh dang tien te
+    public static String formatCurrency(int amount) {
+        NumberFormat formatter = NumberFormat.getInstance(Locale.US); // Định dạng theo US (1,000,000)
+        return formatter.format(amount);
+    }
+    
+    public static void main(String[] args) {
+        
+    }
 }
